2005000169ccf00d6227a06c4558d527f9bde2fe
luizfernandorodrigues <luiz@jmti.com.br>
2019-04-15 23:00:16 -0300

    Adicionar arquivos de projeto.

---
+++ AgiliFood/Models/Contexto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AgiliFood.Models
+{
+    public partial class Contexto : DbContext
+    {
+        public Contexto()
+                    : base("EFConnectionString") { }
+
+        public virtual DbSet<Pais> Pais { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
---
+++ AgiliFood/Models/Pais.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace AgiliFood.Models
+{
+    [Table("Pais")]
+    public class Pais
+    {
+        [Required(ErrorMessage ="id é obrigatório")]
+        [Column("id")]
+        public Guid Id { get; set; }
+
+        [StringLength(10)]
+        [Column("codigo")]
+        public string Codigo { get; set; }
+
+        [Required(ErrorMessage = "Nome é Obrigatório!")]
+        [StringLength(100)]
+        [Column("nome")]
+        public string Nome { get; set; }
+
+        [Required]
+        [Column("timestamp")]
+        public DateTime TimesTamp { get; set; }
+
+    }
+}
